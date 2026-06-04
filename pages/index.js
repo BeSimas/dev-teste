299c6499e5bea167d6cb2a323028a73f0f11ab3f
bernardo <simassouzabernardo@gmail.com>
2026-06-04 04:45:22 +0000

    aqui tem 2 paginas que vao ter sites

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home () {
-    return <h1>pagina inicial do site</h1>
+    return <h1>pagina 
+        inicial 
+        do 
+        site</h1>
+
+
 }
 
 export default Home;
---
+++ pages/segunda_pagina.js
@@ -0,0 +1,10 @@
+function Home () {
+    return <h1> 
+        olha aqui a segunda pagina do site      
+         
+        </h1>
+
+
+}
+
+export default Home;
